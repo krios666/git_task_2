fb56626bc7f88aaf1eb9aad26e44b4c0e58561e6
krios666 <egorkaspilko@gmail.com>
2026-05-17 22:32:59 +0400

    В данном коде из исходной очереди удаляются повторяющиеся, идущие подряд элементы и оставляется только их первое вхождение.

---
+++ programming/стеки и очереди 2.cpp
@@ -0,0 +1,53 @@
+﻿//#include <iostream>
+//#include <queue>
+//using namespace std;
+//
+//void printQueue(queue<int> q) { 
+//    while (!q.empty()) {
+//        cout << q.front() << " ";
+//        q.pop();
+//    }
+//    cout << endl;
+//}
+//
+//int main() {
+//    setlocale(LC_ALL, "RUS");
+//    queue<int> q;      // исходная очередь
+//    queue<int> result; // результирующая очередь
+//    int n, x;
+//
+//    cout << "Введите количество элементов: "; cin >> n;
+//
+//    if (n == 0) {
+//        cout << "Очередь нулевого размера";
+//        return 0;
+//    }
+//
+//    cout << "Введите " << n << " целых чисел: ";
+//
+//    for (int i = 0; i < n; i++) {
+//        cin >> x;
+//        q.push(x);
+//    }
+//
+//    if (q.empty()) {
+//        cout << "Очередь пуста!" << endl;
+//        return 0;
+//    }
+//
+//    int first = q.front();  // запоминаем первый элемент
+//    result.push(first);     // добавляем его в результат
+//    q.pop();               // удаляем из исходной очереди
+//    while (!q.empty()) {
+//        int current = q.front();
+//        q.pop();
+//        if (current != first) {  // если элемент отличается от предыдущего
+//            result.push(current); // добавляем в результат
+//            first = current;       // обновляем предыдущий элемент
+//        } 
+//    }
+//
+//    cout << "Результат: ";
+//    printQueue(result);
+//    return 0;
+//}
